55d789f2782ba22a96a1498f4c7a75c8b2893b07
James Devereux <devereuxjj@gmail.com>
2022-01-23 20:36:26 +0900

    fixed layout on mobile

--- src/App.js
+++ src/App.js
@@ -1,36 +1,28 @@
-import React, { useContext } from 'react'
-import { Header } from './components/Header'
-import { motion } from 'framer-motion'
-import { Outlet, Link } from 'react-router-dom'
-import { UserContext } from './UserContext'
-import Login from './routes/login'
+import React, { useContext } from "react";
+import { Header } from "./components/Header";
+import { motion } from "framer-motion";
+import { Outlet, Link } from "react-router-dom";
+import { UserContext } from "./UserContext";
+import Login from "./routes/login";
 
 export default function App() {
-  const { user, setuser } = useContext(UserContext)
+  const { user, setuser } = useContext(UserContext);
 
   const articles = () => (
     <>
-    <Header />
-    <div className="w-full flex justify-center">
-    <Link className="" to="/articles">
-      Articles
-    </Link>
-  </div>
-  </>
-  )
+      <Header />
+      <div className="w-full flex justify-center">
+        <Link className="" to="/articles">
+          Articles
+        </Link>
+      </div>
+    </>
+  );
 
   return (
     <>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 2 }}
-      >
-        { window.location.pathname === '/word-me-up' ? '' :
-        {articles}
-        }
-        <Outlet />
-      </motion.div>
-    </> 
-  )
+      {window.location.pathname === "/word-me-up" ? "" : { articles }}
+      <Outlet />
+    </>
+  );
 }
--- src/routes/wordmeup.jsx
+++ src/routes/wordmeup.jsx
@@ -1,39 +1,30 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef, useContext } from "react";
 import React from "react";
 
-const ROWONE = [
-  "Q",
-  "W",
-  "E",
-  "R",
-  "T",
-  "Y",
-  "U",
-  "I",
-  "O",
-  "P",
-];
+const ROWONE = ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"];
 
 const ROWTWO = ["A", "S", "D", "F", "G", "H", "J", "K", "L"];
 
 const ROWTHREE = ["Z", "X", "C", "V", "B", "N", "M"];
 
-const validLetters = [ ...ROWONE, ...ROWTWO, ...ROWTHREE ]
+const validLetters = [...ROWONE, ...ROWTWO, ...ROWTHREE];
+
+const GameContext = React.createContext();
 
 const Letter = () => {
+  const letterValue = useContext(GameContext);
+  console.log(letterValue);
   return (
-    <input
+    <div
       type="text"
-      className="text-gray-50 text-3xl font-bold border-2 border-gray-300 h-16 w-16 flex justify-center items-center"
-
-
+      className="text-gray-50 text-3xl font-bold border-2 border-gray-300 h-14 w-14 flex grow shrink justify-center items-center"
     />
   );
 };
 
 const LetterRow = () => {
   return (
-    <div className="grid grid-cols-5 gap-1 flex-none">
+    <div className="grid grid-cols-5 gap-x-2 gap-y-4">
       {Array.from(Array(5).keys()).map((number) => (
         <Letter key={number} />
       ))}
@@ -42,19 +33,8 @@ const LetterRow = () => {
 };
 
 function GameBox() {
-
-  const handleKeyUp = () => {
-    document.addEventListener('keyup', (e) => {
-      if (validLetters.includes(e.key.toUpperCase())) {
-        console.log(e.key)
-      }
-    })
-  }
-
-  handleKeyUp();
-  
   return (
-    <div className="flex justify-center grid grid-rows-5 gap-1">
+    <div className="flex justify-center grid grid-rows-5 gap-1 m-4">
       {Array.from(Array(6).keys()).map((number) => (
         <LetterRow key={number} />
       ))}
@@ -80,14 +60,13 @@ function Header() {
 
 function KeyboardLetter(props) {
   return (
-    <div className="w-12 h-16 text-sm font-bold bg-gray-200 flex items-center justify-center rounded-md m-1">
+    <div className="w-12 h-10 md:h-14 text-xs md:text-sm font-bold bg-gray-200 flex grow shrink items-center justify-center rounded-md m-1">
       {props.letter}
     </div>
   );
 }
 
 function KeyboardLetters() {
-
   return (
     <>
       <div className="w-full flex justify-center">
@@ -95,19 +74,19 @@ function KeyboardLetters() {
           <KeyboardLetter key={letter} letter={letter} />
         ))}
       </div>
-      <div className="w-full flex justify-center">
+      <div className="w-full flex justify-center px-4">
         {ROWTWO.map((letter) => (
           <KeyboardLetter key={letter} letter={letter} />
         ))}
       </div>
       <div className="w-full flex justify-center">
-        <div className="w-12 h-16 text-sm font-bold bg-gray-200 flex-1 flex items-center justify-center rounded-md m-1">
+        <div className="w-12  h-10 md:h-14 text-sm font-bold bg-gray-200 flex-1 flex grow items-center justify-center rounded-md m-1">
           ENTER
         </div>
         {ROWTHREE.map((letter) => (
           <KeyboardLetter key={letter} letter={letter} />
         ))}
-        <div className="w-12 h-16 text-sm font-bold bg-gray-200 flex-1 flex items-center justify-center rounded-md m-1">
+        <div className="w-12  h-10 md:h-14 text-sm font-bold bg-gray-200 flex-1 flex grow items-center justify-center rounded-md m-1">
           DEL
         </div>
       </div>
@@ -117,23 +96,33 @@ function KeyboardLetters() {
 
 function Keyboard() {
   return (
-    <div className="mx-auto">
+    <div className="px-3">
       <KeyboardLetters />
     </div>
   );
 }
 
 export default function WordMeUp() {
-  const [state, setState] = useState([{ answer: "hello" }]);
-  useEffect(() => {});
+  const [gameState, setGameState] = useState([{ answer: "hello", letter: "" }]);
 
+  const handleKeyUp = () => {
+    document.addEventListener("keyup", (e) => {
+      if (validLetters.includes(e.key.toUpperCase())) {
+        setGameState(e.key);
+      }
+    });
+  };
+
+  handleKeyUp();
+
+  useEffect(() => {});
   return (
-    <div className="w-full mx-auto">
-      <div className="h-screen flex flex-col justify-between py-4">
+    <GameContext.Provider value={gameState}>
+      <div className="max-w-3xl mx-auto h-screen flex flex-col justify-between py-4">
         <Header />
         <GameBox />
         <Keyboard />
       </div>
-    </div>
+    </GameContext.Provider>
   );
 }
