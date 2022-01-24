ea8e83e9f41c174de49534db56ddd4d86ed95b4f
James Devereux <devereuxjj@gmail.com>
2022-01-24 18:21:34 +0900

    added frontpage with profile info

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Header } from "./components/Header";
 import { Outlet, Link } from "react-router-dom";
 import { motion } from "framer-motion";
@@ -8,21 +8,46 @@ export default function App() {
     <>
       <div className="h-screen max-w-3xl mx-auto">
         <Header />
-        <motion.span
-          animate={{ opacity: 1 }}
-          transition={{ delay: 4, duration: 1 }}
-          initial={{ opacity: 0 }}
-          className="text-2xl mt-8 text-slate-900	tracking-loose"
-        >
-          I'm a developer who loves language. In a previous lifetime I was an
-          English language teacher.
-        </motion.span>
+        <div className="text-2xl mt-8 text-slate-900	tracking-loose font-light">
+          I'm a{" "}
+          <a href="https://github.com/jjmountain" target="_blank">
+            software engineer{" "}
+          </a>
+          who loves language and learning.
+        </div>
+        <div className="text-xl mt-4 text-slate-900	tracking-loose font-light">
+          私は言語と学習が大好きな
+          <a href="https://github.com/jjmountain" target="_blank">
+            ソフトウェアエンジニア
+          </a>
+          です。
+        </div>
+        <div className="text-xl mt-4 text-slate-900	tracking-loose font-light">
+          저는 언어와 학습을 사랑하는{" "}
+          <a href="https://github.com/jjmountain" target="_blank">
+            소프트웨어 엔지니어
+          </a>
+          입니다.
+        </div>
 
-        <ul>
-          <li className="">
-            I made this in my free time <Link to="/word-me-up">Wordle</Link>{" "}
-            clone in my free time.
+        <h1 className="text-slate-900 mt-12">Some projects I've worked on</h1>
+        <ul className="list-disc pl-6">
+          <li className="text-lg my-2">
+            I built{" "}
+            <a href="https://www.finnly.io/" target="_blank">
+              this landing page
+            </a>{" "}
+            for a crytpo-trading startup
           </li>
+          <li className="text-lg">
+            I made this <Link to="/word-me-up">Wordle</Link> clone with React
+          </li>
+        </ul>
+
+        <h1 className="text-slate-900">Some other stuff</h1>
+        <ul className="list-disc pl-6">
+          <li className="text-lg my-2">A list of my favourite Korean films.</li>
+          <li className="text-lg"></li>
         </ul>
       </div>
     </>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,19 @@
 import React from "react";
+import parrot from "../icons8-parrot-96.png";
+
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
     <Link className="no-underline" to="/">
-      <motion.div
-        animate={{ opacity: 1 }}
-        transition={{ delay: 1, duration: 1 }}
-        initial={{ opacity: 0 }}
-        className=" sm:text-6xl text-5xl  mt-8 text-slate-800	font-semibold tracking-loose"
-      >
-        Hi. I'm James.
-      </motion.div>
-      <motion.div className="text-xs sm:text-sm uppercase text-center mb-8 text-gray-600	opacity-90 font-light"></motion.div>
+      <div className="flex items-center pt-20">
+        <img src={parrot} className="inline w-14" alt="parrot" />
+        <div className="ml-4 sm:text-6xl text-5xl text-slate-800 inline font-semibold tracking-loose">
+          Hi. I'm James.
+        </div>
+        <motion.div className="text-xs sm:text-sm uppercase text-center mb-8 text-gray-600	opacity-90 font-light"></motion.div>
+      </div>
     </Link>
   );
 };
